1ec373d813a46add1abc2a33e2957fc65e5edbf2
exemee <dimamakhnin45640@gmail.com>
2023-02-13 17:06:26 +0300

    initial commit

---
+++ src/main/java/com/example/volhv/VolhvApplication.java
@@ -0,0 +1,13 @@
+package com.example.volhv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VolhvApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VolhvApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/volhv/service/DBService.java
@@ -0,0 +1,9 @@
+package com.example.volhv.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class DBService {
+
+
+}
